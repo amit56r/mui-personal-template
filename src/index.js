6d51055f0c5e8704899cb8bbd41acfe9e9f8f1ce
Amit Roy <amit56r@gmail.com>
2019-04-27 16:17:48 -0700

    Init and getting starter files ready

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
